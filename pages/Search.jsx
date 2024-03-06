2541a765d2c920f3a87dd0c279b651036e3069a1
carey-andrew <carey_andrew@hotmail.com>
2024-03-06 12:12:29 +0000

    filepath edit

--- pages/Search.jsx
+++ pages/Search.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Header from "../app/components/header/Header";
 import Footer from "../app/components/footer/Footer";
-import SearchMovie from "../app/components/searchAll/SearchMovie";
 import GenreSearch from "@/app/components/GenreSearch/GenreSearch";
 import "../app/globals.css";
 
